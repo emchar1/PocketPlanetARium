0279d17516fe4eb87b627744b304332a9efeeba7
Eddie Char <emchar1@gmail.com>
2020-09-21 11:31:23 -0700

    Rename project

---
+++ PocketPlanetARium/Controllers/PlanetARiumController.swift
@@ -0,0 +1,106 @@
+//
+//  PlanetARiumController.swift
+//  PocketPlanetARium
+//
+//  Created by Eddie Char on 9/14/20.
+//  Copyright © 2020 Eddie Char. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class PlanetARiumController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var scaleSlider: UISlider!
+    
+    var planetarium = PlanetARium()
+    
+    //Pinch to zoom properties
+    var pinchBegan: CGFloat?
+    var pinchChanged: CGFloat?
+    var scaleValue: Float = 0.2 {
+        didSet {
+            scaleValue = scaleValue.clamp(min: 0, max: 1)
+            scaleSlider.value = scaleValue
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scaleSlider.value = scaleValue
+        scaleSlider.setThumbImage(UIImage(systemName: "hare.fill"), for: .normal)
+
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = false
+        
+        planetarium.addPlanets(scale: scaleValue, toNode: sceneView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    
+    // MARK: - UI Controls
+    
+    @IBAction func scaleChanged(_ sender: UISlider) {
+        scaleValue = sender.value
+        
+        planetarium.addPlanets(scale: scaleValue, toNode: sceneView)
+//        planetarium.scalePlanets(to: sender.value)
+    }
+    
+    
+    // MARK: - Gesture Interaction
+    
+    @IBAction func handlePinch(_ sender: UIPinchGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            pinchBegan = sender.scale
+        case .changed:
+            pinchChanged = sender.scale
+        case .ended:
+            //reset values
+            pinchBegan = nil
+            pinchChanged = nil
+        default:
+            break
+        }
+        
+        if let began = pinchBegan, let changed = pinchChanged {
+            let diff = Float(changed - began)
+            scaleValue += diff / (diff < 0 ? 100 : 200)
+            planetarium.addPlanets(scale: scaleValue, toNode: sceneView)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        planetarium.pauseAnimation()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        planetarium.setSpeed(to: scaleValue)
+    }
+
+}
+
+
+// MARK: - AR Scene View Delegate
+    
+extension PlanetARiumController: ARSCNViewDelegate {
+    
+}
---
+++ PocketPlanetARium/Models/PlanetARium.swift
@@ -0,0 +1,275 @@
+//
+//  PlanetARium.swift
+//  PocketPlanetARium
+//
+//  Created by Eddie Char on 9/18/20.
+//  Copyright © 2020 Eddie Char. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+struct PlanetARium {
+    private var sun: Planet?
+    private var moon: Planet?
+    private var planets: [String: Planet] = [:]
+    
+    //Don't touch these values!!
+    private let scaleFactor: Float = 3
+    private let scaleMinimum: Float = 0.123
+    private let scaleMaximum: Float = 1
+    
+    
+    // MARK: - Add/Remove Planets to Scene
+    
+    /**
+     Adds the solar system to the sceneView, and animates them in an intuitive way with respect to size, scale, and speed.
+     - parameters:
+     - scale: solar system scale with respect to size and orbital distance; works well with values between 0 to 1, i.e. slider values
+     - topSpeed: max speed of the animation
+     - toNode: the scene view to add the solar system to
+     - The higher the topSpeed, the faster the animation. Suggested values: 2, 4, 8, 16, 32, 64, 128, 256
+     */
+    mutating func addPlanets(scale: Float, topSpeed speed: TimeInterval = 128, toNode sceneView: ARSCNView) {
+        let adjustedScale = pow(scale.clamp(min: scaleMinimum, max: scaleMaximum), scaleFactor)
+        
+        self.addPlanets(earthRadius: adjustedScale * 3,
+                        earthDistance: adjustedScale * -20,
+                        earthDay: 1 / speed,
+                        earthYear: 365 / speed,
+                        toNode: sceneView)
+        
+        setSpeed(to: scale)
+    }
+    
+    /**
+     Adds the solar system to the sceneView, and animates them.
+     - parameters:
+     - earthRadius: size of the Earth, in meters
+     - earthDistance: distance from the Earth to the sun's center, in meters
+     - earthYear: length of time it takes for the Earth to make one revolution around the sun, in seconds
+     - sceneView: the scene view to add the solar system to
+     - This function allows for more independent customization regarding size of planets, orbital distances, and speed of animation.
+     */
+    mutating func addPlanets(earthRadius: Float, earthDistance: Float, earthDay: TimeInterval, earthYear: TimeInterval, toNode sceneView: ARSCNView) {
+        //Clears the scene view before setting up the solar system.
+        sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
+            node.removeFromParentNode()
+        }
+
+        //Set up the solar system
+        sun = Planet(name: "Sun",
+                     radius: (abs(earthDistance) * 0.2).clamp(min: 0.008, max: 0.02),
+                     tilt: SCNVector3(x: 0, y: 0, z: 0),
+                     position: SCNVector3(0, 0, -0.2),
+                     rotationSpeed: earthDay * 27)
+        
+        guard let sun = sun else {
+            print("Sun was nil (this should not happen).")
+            return
+        }
+
+        
+        planets["Mercury"] = Planet(name: "Mercury",
+                                    radius: earthRadius * 0.38,
+                                    tilt: SCNVector3(x: 0, y: 0, z: 0),
+                                    aPosition: (K.degToRad(5), earthDistance * 0.39),
+                                    rotationSpeed: earthDay * 58,
+                                    orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                    orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                    orbitalCenterRotationSpeed: earthYear * 0.24)
+        
+        planets["Venus"] = Planet(name: "Venus",
+                                  radius: earthRadius * 0.95,
+                                  tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(177)),
+                                  aPosition: (K.degToRad(25), earthDistance * 0.72),
+                                  rotationSpeed: earthDay * 243,
+                                  orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                  orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                  orbitalCenterRotationSpeed: earthYear * 0.62)
+        
+        //easter egg nested Venus surface skin
+        if let venus = planets["Venus"] {
+            planets["Venus_Surface"] = Planet(name: "Venus_Surface",
+                                              radius: venus.getRadius() * 0.25,
+                                              tilt: venus.getTilt(),
+                                              position: venus.getNode().position,
+                                              rotationSpeed: venus.getRotationSpeed(),
+                                              orbitalCenterTilt: venus.getOrbitalCenterTilt(),
+                                              orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                              orbitalCenterRotationSpeed: venus.getOrbitalCenterRotationSpeed())
+        }
+        
+        planets["Earth"] = Planet(name: "Earth",
+                                  radius: earthRadius,
+                                  tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(23)),
+                                  aPosition: (K.degToRad(50), earthDistance),
+                                  rotationSpeed: earthDay,
+                                  orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                  orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                  orbitalCenterRotationSpeed: earthYear)
+        
+        //Add earth's moon.
+        if let earth = planets["Earth"] {
+            moon = Planet(name: "Moon",
+                          radius: earthRadius * 0.25,
+                          tilt: SCNVector3(x: 0, y: .pi, z: K.degToRad(6.9)),
+                          position: SCNVector3(0, 0, earthRadius + earthRadius * 0.5),
+                          rotationSpeed: 9999,
+                          orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: K.degToRad(5.1)),
+                          orbitalCenterPosition: earth.getNode().position,
+                          orbitalCenterRotationSpeed: earthDay * 27)
+            
+            if let moon = moon {
+                earth.addSatellite(moon)
+            }
+        }
+        
+        planets["Mars"] = Planet(name: "Mars",
+                                 radius: earthRadius * 0.53,
+                                 tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(25)),
+                                 aPosition: (K.degToRad(70), earthDistance * 1.52),
+                                 rotationSpeed: earthDay * 1.04,
+                                 orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                 orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                 orbitalCenterRotationSpeed: earthYear * 1.88)
+        
+        planets["Jupiter"] = Planet(name: "Jupiter",
+                                    radius: earthRadius * 11.21,
+                                    tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(3)),
+                                    aPosition: (K.degToRad(100), earthDistance * 5.2),
+                                    rotationSpeed: earthDay * 0.42,
+                                    orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                    orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                    orbitalCenterRotationSpeed: earthYear * 11.87)
+        
+        planets["Saturn"] = Planet(name: "Saturn",
+                                   radius: earthRadius * 9.45,
+                                   tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(27)),
+                                   aPosition: (K.degToRad(150), earthDistance * 9.58),
+                                   rotationSpeed: earthDay * 0.46,
+                                   orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                   orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                   orbitalCenterRotationSpeed: earthYear * 29.44)
+        
+        if let saturn = planets["Saturn"] {
+            saturn.addRings(imageFileName: "saturn_ringsl", innerRadius: saturn.getRadius() * 1.1, outerRadius: saturn.getRadius() * 2.3)
+        }
+        
+        planets["Uranus"] = Planet(name: "Uranus",
+                                   radius: earthRadius * 4.01,
+                                   tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(98)),
+                                   aPosition: (K.degToRad(180), earthDistance * 19.18),
+                                   rotationSpeed: earthDay * 0.71,
+                                   orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                   orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                   orbitalCenterRotationSpeed: earthYear * 83.81)
+        
+        if let uranus = planets["Uranus"] {
+            uranus.addRings(imageFileName: "noimg", innerRadius: uranus.getRadius() * 2, outerRadius: uranus.getRadius() * 2)
+        }
+        
+        planets["Neptune"] = Planet(name: "Neptune",
+                                    radius: earthRadius * 3.88,
+                                    tilt: SCNVector3(x: 0, y: 0, z: K.degToRad(23)),
+                                    aPosition: (K.degToRad(195), earthDistance * 30.03),
+                                    rotationSpeed: earthDay * 0.67,
+                                    orbitalCenterTilt: SCNVector3(x: 0, y: 0, z: 0),
+                                    orbitalCenterPosition: sun.getOrbitalCenterNode().position,
+                                    orbitalCenterRotationSpeed: earthYear * 163.84)
+        
+        
+        //Animate the solar system!
+        if let moon = moon {
+            moon.animate()
+        }
+        
+        for (_, planet) in planets {
+            planet.animate()
+            sun.addSatellite(planet)
+            
+            planet.addOrbitPath()
+        }
+        
+        sun.animate()
+        sun.addLightSource(omniLumens: 1000, ambientLumens: 300)
+
+        sceneView.scene.rootNode.addChildNode(sun.getOrbitalCenterNode())
+    }
+    
+    
+    // MARK: - Speed controls
+    
+    //***********TEST***********************
+    func scalePlanets(to scale: Float) {
+        let adjustedScale = pow(scale.clamp(min: scaleMinimum, max: scaleMaximum), scaleFactor)
+        
+        for (_, planet) in planets {
+            let node = planet.getNode()
+            
+            node.simdScale = SIMD3(x: adjustedScale, y: adjustedScale, z: adjustedScale)
+            //            node.simdWorldPosition = simd_float3(x: adjustedScale, y: adjustedScale, z: adjustedScale)
+        }
+        
+        if let moon = moon {
+            let node = moon.getNode()
+            
+            node.simdScale = SIMD3(x: adjustedScale, y: adjustedScale, z: adjustedScale)
+            //            node.simdPosition = SIMD3(x: adjustedScale * node.position.x, y: adjustedScale * node.position.y, z: adjustedScale * node.position.z)
+        }
+        
+        setSpeed(to: scale)
+    }
+    
+    /**
+     Sets the speed of the animation to the given input value.
+     - parameter speed: range from 0 to 1, with 1 being actual speed
+     */
+    func setSpeed(to speed: Float) {
+        let adjustedSpeed = pow((1 - speed).clamp(min: scaleMinimum, max: scaleMaximum), scaleFactor)
+        
+        for action in getAllActions() {
+            action.speed = CGFloat(adjustedSpeed)
+        }
+    }
+    
+    /**
+     Pauses all animations.
+     */
+    func pauseAnimation() {
+        for action in getAllActions() {
+            action.speed = CGFloat(pow(scaleMinimum, scaleFactor + 0.75))
+        }
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    /**
+     Iterates through all planet objects and returns all current SCNActions.
+     */
+    private func getAllActions() -> [SCNAction] {
+        var actions = [SCNAction]()
+        
+        if let sun = sun {
+            for action in sun.getAllPlanetActions() {
+                actions.append(action)
+            }
+        }
+        
+        for (_, planet) in planets {
+            for action in planet.getAllPlanetActions() {
+                actions.append(action)
+            }
+        }
+        
+        if let moon = moon {
+            for action in moon.getAllPlanetActions() {
+                actions.append(action)
+            }
+        }
+        
+        return actions
+    }
+}
